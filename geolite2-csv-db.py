34b3bc754b9bfcde908a37b349b2e56e8afc51b1
Xiongfei Shi <jenson.shixf@gmail.com>
2017-04-21 14:00:56 +0800

    modify comments

--- geolite2-csv-db.py
+++ geolite2-csv-db.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-    Copyright (c) 2017 Jenson Shi <jenson.shixf@gmail.com>
+    Copyright (c) 2017 Xiongfei Shi <jenson.shixf@gmail.com>
     
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
